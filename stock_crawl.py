3af67c0851e74bf07e0d59f4e2b9706e3d11f7ba
unknown <ngocnam3518@gmail.com>
2021-02-04 15:16:44 +0700

    print iteracter

--- stock_crawl.py
+++ stock_crawl.py
@@ -5,7 +5,8 @@ from utils import pdate2date, save
 URLcafef = "https://cafef.vn"
 URL = 'https://cafef.vn/timeline/31/trang-1.chn'
 
-for i in range(1, 2):
+for i in range(1913, 2000):
+    print(i, "--------------------------")
     URL = 'https://cafef.vn/timeline/31/trang-' + str(i) +'.chn'
     page = requests.get(URL)
 
@@ -19,22 +20,22 @@ for i in range(1, 2):
         URLpage = URLcafef + link
         article = requests.get(URLpage)
         soup2 = BeautifulSoup(article.content, 'html.parser')
-        # try:
-        date = soup2.findAll("span", {"class":"pdate"})[0]
-        date = pdate2date(date.text)
+        try:
+            date = soup2.findAll("span", {"class":"pdate"})[0]
+            date = pdate2date(date.text)
 
-        sumary = soup2.findAll("h2", {"class":"sapo"})[0].text.strip()
-        results2 = soup2.find(id="mainContent")
-        all_p = results2.findAll('p')
-        main_content = sumary
-        for p in all_p:
-            main_content += p.text
-        # print(main_content + "\n----------------------------------------")
-        
-        #save file
-        data_foder = "crawl-data"
-        filename = link.replace("/", "") + ".txt"
-        save(filename, main_content, date, data_foder)
-        # except:
-        #     pass
+            sumary = soup2.findAll("h2", {"class":"sapo"})[0].text.strip()
+            results2 = soup2.find(id="mainContent")
+            all_p = results2.findAll('p')
+            main_content = sumary
+            for p in all_p:
+                main_content += p.text
+            # print(main_content + "\n----------------------------------------")
+            
+            #save file
+            data_foder = "crawl-data"
+            filename = link.replace("/", "") + ".txt"
+            save(filename, main_content, date, data_foder)
+        except:
+            print("error:", link)
 
